198a56d1aa319daa1de0edad342699ab7df3eed9
Sarthak <sarthakrawatbiz@gmail.com>
2025-05-17 00:04:21 +0530

    Fixed the SPA problem

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const port = 3000;
 
 const settings = {
   apiKey: process.env.ALCHEMY_API_KEY,
-  network: Network.BASE_SEPOLIA,
+  network: Network.BASE_MAINNET,
 };
 
 export const alchemy = new Alchemy(settings);
@@ -33,44 +33,6 @@ mongoose.connect(
 //Routes
 app.use("/user", userRouter);
 app.use("/nft", nftRouter);
-
-app.get("/getData", async (req, res) => {
-  const tokenURI = req.query.uri;
-  const decodedUrl = decodeURIComponent(tokenURI);
-  try {
-    const response = await axios.get(decodedUrl);
-    const data = response.data;
-    res.json(data);
-  } catch (error) {
-    console.error("Error fetching NFT data:", error);
-    res.status(500).json({ error: "Failed to fetch NFT data" });
-  }
-});
-
-app.get("/nftData", async (req, res) => {
-  const { uri, address } = req.query;
-
-  try {
-    // Fetch NFT metadata
-    const response = await axios.get(uri);
-    const nftData = response.data;
-
-    // Fetch additional data (e.g., number of owners) if needed
-    const numberOfOwners = await alchemyFetch(address);
-
-    // Combine data
-    const data = {
-      name: nftData.name,
-      image: nftData.image_url,
-      artist: nftData.creator,
-    };
-
-    res.json(data);
-  } catch (error) {
-    console.error("Error fetching NFT data:", error);
-    res.status(500).json({ error: "Failed to fetch NFT data" });
-  }
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
