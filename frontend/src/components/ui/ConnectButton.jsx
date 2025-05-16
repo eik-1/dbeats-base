068ca8597e266d071915e55adc5d5a22a3cc9cea
Sarthak <sarthakrawatbiz@gmail.com>
2025-05-17 00:30:04 +0530

    App sending to profile page on every reload

--- frontend/src/components/ui/ConnectButton.jsx
+++ frontend/src/components/ui/ConnectButton.jsx
@@ -28,7 +28,6 @@ export default function ConnectButton() {
                     }
                     await createUser(newUser)
                 }
-                navigate("/app/profile")
             }
         }
         initializeUser()
